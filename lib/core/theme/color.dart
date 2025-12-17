786d2122d8bafaf2392ad6a7358f8dd38a5c0315
aiden30015 <loolloo080313@gmail.com>
2025-12-17 15:23:12 +0900

    🎨:: CotamColors 클래스 추가 및 색상 정의

---
+++ lib/core/theme/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CotamColors {
+  CotamColors._();
+
+  // Primary Colors
+  static const Color primary500 = Color(0xFF32AB7B);
+  static const Color primary400 = Color(0xFF31DC98);
+
+  // Neutral Colors
+  static const Color neutral900 = Color(0xFF181B18);
+  static const Color neutral800 = Color(0xFF313231);
+  static const Color neutral420 = Color(0xFF9AA29A);
+  static const Color neutral300 = Color(0xFFAFB6AF);
+  static const Color neutral20 = Color(0xFFFAFAFA);
+}
